05bb2e03f8115c8e3b37583f86e0b08ca8360b3d
Michael (Parker) Parker <parkervcp@gmail.com>
2025-05-24 09:12:07 -0400

    update mumble (#303)
    * update mumble
    
    mumble now ships their own docker image
    updating ours to be based on that.
    
    * update entrypoint
    
    uses the mumble executable to print the current version

--- voice/mumble/entrypoint.sh
+++ voice/mumble/entrypoint.sh
@@ -6,8 +6,7 @@ INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
 # Print the Muble server version
-echo -e "Mumble release version:"
-cat /usr/local/share/mumble/latest_tag.txt
+/usr/bin/mumble-server --version
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
